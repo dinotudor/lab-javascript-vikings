f80caf5554f4c5f3b96a77a174717302d3737643
Dino Tudor <dinotudor@gmail.com>
2019-07-23 22:14:04 +0200

    second subclass added

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,4 +1,3 @@
-// Soldier
 class Soldier {
   constructor(health, strength) {
     this.health = health;
@@ -13,19 +12,40 @@ Soldier.prototype.attack = function () {
 Soldier.prototype.receiveDamage = function(damage) {
   this.health -= damage;
 }
-// Viking
-class Viking {
+
+
+class Viking extends Soldier {
   constructor(name, health, strength) {
+    super(health, strength);
     this.name = name;
+  }
+  receiveDamage (damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return`${this.name} has received ${damage} points of damage`;
+    } else if (this.health <= 0) {
+      return `${this.name} has died in act of combat`;
+    }
+  }
+  battleCry() {
+    return `Odin Owns You All!`
+  }
+}
+
+class Saxon extends Soldier {
+  constructor(health, strength) {
+    super(health, strength)
     this.health = health;
     this.strength = strength;
   }
+  receiveDamage(damage){
+    this.health -= damage;
+    if (this.health > 0) {
+      return `A Saxon has received ${damage} points of damage`
+    } else if (this.health <= 0) {
+      return `A Saxon has died in combat`
+    }
+  }
 }
-Viking.prototype = Object.create(Soldier.prototype);
-Viking.prototype.constructor = Viking;
 
-// Saxon
-class Saxon {}
-
-// War
 class War {}
