00bff4e05d4d642c03d4dd7ae5adc7519d29310f
Dino Tudor <dinotudor@gmail.com>
2019-07-23 18:17:50 +0200

    iteration 2,3

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,28 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+}
 
+Soldier.prototype.attack = function () {
+  return this.strength
+}
+
+Soldier.prototype.receiveDamage = function(damage) {
+  this.health -= damage;
+}
 // Viking
-class Viking {}
+class Viking {
+  constructor(name, health, strength) {
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+  }
+}
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
 
 // Saxon
 class Saxon {}
