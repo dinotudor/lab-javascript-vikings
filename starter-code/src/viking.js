2ae54b6f02a5e2cac6a971c6bc4341c1b994d49d
Dino Tudor <dinotudor@gmail.com>
2019-07-23 23:51:39 +0200

    second subclass added

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -3,28 +3,25 @@ class Soldier {
     this.health = health;
     this.strength = strength;
   }
+  attack = function () {
+    return this.strength
+  }
+  receiveDamage = function(damage) {
+    this.health -= damage;
+  }
 }
-
-Soldier.prototype.attack = function () {
-  return this.strength
-}
-
-Soldier.prototype.receiveDamage = function(damage) {
-  this.health -= damage;
-}
-
 
 class Viking extends Soldier {
   constructor(name, health, strength) {
     super(health, strength);
     this.name = name;
   }
-  receiveDamage (damage) {
+  receiveDamage(damage) {
     this.health -= damage;
     if (this.health > 0) {
-      return`${this.name} has received ${damage} points of damage`;
-    } else if (this.health <= 0) {
-      return `${this.name} has died in act of combat`;
+      return `${this.name} has received ${damage} points of damage`
+    } else {
+      return `${this.name} has died in act of combat`
     }
   }
   battleCry() {
@@ -35,10 +32,8 @@ class Viking extends Soldier {
 class Saxon extends Soldier {
   constructor(health, strength) {
     super(health, strength)
-    this.health = health;
-    this.strength = strength;
   }
-  receiveDamage(damage){
+  receiveDamage(damage) {
     this.health -= damage;
     if (this.health > 0) {
       return `A Saxon has received ${damage} points of damage`
@@ -48,4 +43,32 @@ class Saxon extends Soldier {
   }
 }
 
-class War {}
+class War {
+  constructor() {
+    this.vikingArmy = [];
+    this.saxonArmy = [];
+  }
+  addViking(Viking){
+    this.vikingArmy.push(Viking)
+  }
+  addSaxon(Saxon){
+    this.saxonArmy.push(Saxon)
+  }
+  vikingAttack(){
+    const saxonIndex = Math.floor(Math.random() * this.saxonArmy.length);
+    const saxon = this.vikingArmy[saxonIndex];
+    const vikingIndex = Math.floor(Math.random() * this.vikingArmy.length);
+    const viking = this.vikingArmy[vikingIndex];
+    const result = saxon.receiveDamage(viking.strength);
+    if (saxon.health < 0) {
+      this.saxonArmy.splice(saxonIndex, 1);
+    }
+    return result
+  }
+  saxonAttack(){
+
+  }
+  showStatus(){
+
+  }
+}
